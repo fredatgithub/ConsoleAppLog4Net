4064761a057ac474687a65f1303fbb9ace843b75
Fred <domaine75@gmail.com>
2023-01-25 16:30:40 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleAppLog4Net/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using log4net;
+using log4net.Config;
+
+namespace ConsoleAppLog4Net
+{
+  internal class Program
+  {
+    static void Main(string[] arguments)
+    {
+      ILog logger = LogManager.GetLogger(typeof(Program));
+      string Log4NetConfigFilePath = "log4net.config.xml";
+      XmlConfigurator.ConfigureAndWatch(new FileInfo(Log4NetConfigFilePath));
+      Action<string> Display = Console.WriteLine;
+      var startOfApplication = "Start of the application";
+      Display(startOfApplication);
+      logger.Info(startOfApplication);
+      Display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
